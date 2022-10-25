200f1c9825547baa6fcfcf7624a3e7b27e05a522
Erick .oG Gutierrez <113930446+ErickG1234@users.noreply.github.com>
2022-10-25 17:31:53 +0000

    how to abstract by using functions and mutiple .py folders

---
+++ main.py
@@ -0,0 +1,22 @@
+from add import add_fruit
+from division import divide_fruit
+from multiply import multiply_fruit
+
+
+apples = int(input("how many apples?"))
+oranges = int(input("how many apples?"))
+
+# function call for adding apples and oranges 
+print(add_fruit(apples, oranges))
+  
+
+
+# function for dividing apples and oranges
+print(divide_fruit(apples, oranges))
+  
+
+
+
+# function for multplying apples and oranges
+print(multiply_fruit(apples, oranges))
+  
